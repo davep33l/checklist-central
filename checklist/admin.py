221066f6b0557e1202d5ecb2e8be6d8e86d4bff6
dave peel <davidpeel33@hotmail.com>
2024-06-27 12:36:13 +0100

    Add base.html connection to templates
    - Added basic base.html template and linked to the existing
    templates within the checklist project to test functionality.

--- checklist/admin.py
+++ checklist/admin.py
@@ -3,7 +3,15 @@ from .models import Department, Team, ChecklistTemplate, TaskTemplate, Checklist
 
 # Register your models here.
 
-admin.site.register(Department)
+# admin.site.register(Department)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_on', 'created_by', 'updated_on', 'updated_by')
+    list_filter = ['created_on', 'created_by', 'updated_on', 'updated_by']
+    search_fields = ['name', 'description']
+
+
 admin.site.register(Team)
 admin.site.register(ChecklistTemplate)
 admin.site.register(TaskTemplate)
