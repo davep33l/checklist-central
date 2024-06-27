06c9304d8ffe5042be7302543fb6993878eafcb8
dave peel <davidpeel33@hotmail.com>
2024-06-27 23:45:06 +0100

    Add allauth for user authentication
    - Add a login and logout to the application

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('checklist.urls'), name='checklist'),
+    path('accounts/', include('allauth.urls')),
 
 ]
